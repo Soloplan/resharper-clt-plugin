d1649fb2b952118e988c8669a1535f819db3a5e8
Thomas Rödel <37671934+lightslategray@users.noreply.github.com>
2018-04-05 08:28:18 +0200

    Add data models for issue and rule definitions
    + Add InspectCodeIssueDefinitionModel.java: the data model for issue definitions of the InspectCode command line tool.
    + Add SonarQubeRuleDefinitionModel.java: the data model for rule definitions of SonarQube.

---
+++ src/main/java/com/soloplan/oss/sonarqube/plugin/resharper/clt/models/InspectCodeIssueDefinitionModel.java
@@ -0,0 +1,271 @@
+package com.soloplan.oss.sonarqube.plugin.resharper.clt.models;
+
+import com.soloplan.oss.sonarqube.plugin.resharper.clt.enumerations.InspectCodeIssueSeverity;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Data model class for {@code InspectCode} issue definitions.
+ */
+public class InspectCodeIssueDefinitionModel {
+
+  // region Member variables
+
+  /** The issue identifier that should be unique within {@code InspectCode}. */
+  private final String issueId;
+
+  /** The category name of the issue within {@code InspectCode}. */
+  private String category;
+
+  /** The category identifier of the issue within {@code InspectCode}. */
+  private String categoryId;
+
+  /** The human-readable description of the {@code InspectCode} issue. */
+  private String description;
+
+  /** Indicates, whether the {@code InspectCode} issue is defined in a global ({@code true}) or local ({@code false}) scope. */
+  private boolean isGlobalRuleDefinition = false;
+
+  /** The severity value as defined by {@code InspectCode}. */
+  private InspectCodeIssueSeverity severity = InspectCodeIssueSeverity.getDefaultSeverity();
+
+  /** The subcategory name of the issue within {@code InspectCode}. */
+  private String subCategory;
+
+  /** A uniform resource locator referring to a website where more information about this issue can be found. */
+  private URL wikiUrl;
+
+  // endregion
+
+  /**
+   * Creates a new instance of the {@link InspectCodeIssueDefinitionModel} class using the supplied {@code issueId} as unique identifier.
+   *
+   * @param issueId
+   *     The unique identifier of this issue as defined by {@code InspectCode}.
+   */
+  public InspectCodeIssueDefinitionModel(@NotNull String issueId) {
+    this.issueId = issueId.trim();
+  }
+
+  /**
+   * Gets the unique identifier of this issue as defined by {@code InspectCode}.
+   *
+   * @return The unique identifier of this issue as defined by {@code InspectCode}.
+   */
+  public String getIssueId() {
+    return issueId;
+  }
+
+  /**
+   * Gets the category name of this issue.
+   *
+   * @return The category name of this issue.
+   */
+  public String getCategory() {
+    return category;
+  }
+
+  /**
+   * Sets the category name of this issue.
+   *
+   * @param category
+   *     The new category name of this issue.
+   */
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  /**
+   * Gets the category identifier of this issue.
+   *
+   * @return The category identifier of this issue.
+   */
+  public String getCategoryId() {
+    return categoryId;
+  }
+
+  /**
+   * Sets the category identifier of this issue.
+   *
+   * @param categoryId
+   *     The new category identifier of this issue.
+   */
+  public void setCategoryId(String categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  /**
+   * Gets the human-readable description of this issue.
+   *
+   * @return The human-readable description of this issue.
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * Sets the human-readable description of this issue.
+   *
+   * @param description
+   *     The new human-readable description of this issue.
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  /**
+   * Gets a {@code boolean} value indicating, whether the issue is defined by {@code InspectCode} in a global ({@code true}) or local
+   * ({@code false}) scope.
+   *
+   * @return A {@code boolean} value indicating, whether the issue is defined by {@code InspectCode} in a global ({@code true}) or local *
+   *     ({@code false}) scope.
+   */
+  public boolean isGlobalRuleDefinition() {
+    return isGlobalRuleDefinition;
+  }
+
+  /**
+   * Sets the {@code boolean} value that indicates whether the issue is defined by {@code InspectCode} in a global ({@code true}) or local
+   * ({@code false}) scope.
+   *
+   * @param globalRuleDefinition
+   *     The new {@code boolean} scope indicator value.
+   */
+  public void setGlobalRuleDefinition(boolean globalRuleDefinition) {
+    this.isGlobalRuleDefinition = globalRuleDefinition;
+  }
+
+  /**
+   * Sets the {@code boolean} value that indicates whether the issue is defined by {@code InspectCode} in a global ({@code true}) or local
+   * ({@code false}) scope by parsing the supplied {@link String} argument to its corresponding {@link Boolean} representation.
+   *
+   * @param booleanValue
+   *     A {@link String} value that will be parsed to its {@link Boolean} representation and set as the new {@code boolean} scope indicator
+   *     value.
+   */
+  public void setGlobalRuleDefinition(String booleanValue) {
+    this.isGlobalRuleDefinition = Boolean.parseBoolean(booleanValue);
+  }
+
+  /**
+   * Gets the severity value of this issue as defined by {@code InspectCode}.
+   *
+   * @return The severity value of this issue as defined by {@code InspectCode}.
+   */
+  public InspectCodeIssueSeverity getSeverity() {
+    return severity;
+  }
+
+  /**
+   * Sets the severity value of this issue as defined by {@code InspectCode}.
+   *
+   * @param severity
+   *     The new severity value of this issue as defined by {@code InspectCode}.
+   */
+  public void setSeverity(InspectCodeIssueSeverity severity) {
+    this.severity = severity;
+  }
+
+  /**
+   * Sets the severity value of this issue by parsing the supplied {@link String} to its corresponding {@link InspectCodeIssueSeverity}
+   * value.
+   *
+   * @param severityValue
+   *     A {@link String} value that will be parsed to its corresponding {@link InspectCodeIssueSeverity} value and set as the new new
+   *     severity value of this issue as defined by {@code InspectCode}.
+   */
+  public void setSeverity(String severityValue) {
+    this.severity = InspectCodeIssueSeverity.fromSeverityValue(severityValue);
+  }
+
+  /**
+   * Gets the sub category name of this issue.
+   *
+   * @return The sub category name of this issue.
+   */
+  public String getSubCategory() {
+    return subCategory;
+  }
+
+  /**
+   * Sets the sub category name of this issue.
+   *
+   * @param subCategory
+   *     The new sub category name of this issue.
+   */
+  public void setSubCategory(String subCategory) {
+    this.subCategory = subCategory;
+  }
+
+  /**
+   * Gets the uniform resource locator, referring to a website where more information about this issue can be found.
+   *
+   * @return The uniform resource locator, referring to a website where more information about this issue can be found.
+   */
+  public URL getWikiUrl() {
+    return wikiUrl;
+  }
+
+  /**
+   * Sets the uniform resource locator by parsing the supplied {@link String} to a valid {@link URL} instance, setting {@link #wikiUrl} to
+   * {@code null} if the supplied {@link String} is not a valid uniform resource locator.
+   *
+   * @param wikiUrl
+   *     The {@link String} representation of the new uniform resource locator, which is converted to a valid {@link URL} instance. If the
+   *     supplied {@link String} is not a valid uniform resource locator, the the internal {@link #wikiUrl} value is set to {@code null}.
+   */
+  public void setWikiUrl(String wikiUrl) {
+    // Reset the member variable if the supplied argument is null or an empty string
+    if (wikiUrl == null || wikiUrl.trim().isEmpty()) {
+      this.wikiUrl = null;
+    } else {
+      try {
+        // Try to parse the supplied string to a valid URL, ignoring any exception
+        this.wikiUrl = new URL(wikiUrl);
+      } catch (MalformedURLException exception) { /* ignore */ }
+    }
+  }
+
+  /**
+   * Sets the uniform resource locator, referring to a website where more information about this issue can be found.
+   *
+   * @param wikiUrl
+   *     The new uniform resource locator, referring to a website where more information about this issue can be found.
+   */
+  public void setWikiUrl(URL wikiUrl) {
+    this.wikiUrl = wikiUrl;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+
+    return this.issueId.equals(((InspectCodeIssueDefinitionModel) other).issueId);
+  }
+
+  @Override
+  public int hashCode() {
+    return this.issueId.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return "InspectCodeIssueDefinitionModel{" +
+        "issueId='" + issueId + '\'' +
+        ", category='" + category + '\'' +
+        ", categoryId='" + categoryId + '\'' +
+        ", description='" + description + '\'' +
+        ", isGlobalRuleDefinition=" + isGlobalRuleDefinition +
+        ", severity=" + severity +
+        ", subCategory='" + subCategory + '\'' +
+        ", wikiUrl=" + wikiUrl +
+        '}';
+  }
+}
---
+++ src/main/java/com/soloplan/oss/sonarqube/plugin/resharper/clt/models/SonarQubeRuleDefinitionModel.java
@@ -0,0 +1,284 @@
+package com.soloplan.oss.sonarqube.plugin.resharper.clt.models;
+
+import com.soloplan.oss.sonarqube.plugin.resharper.clt.enumerations.SonarQubeRuleDescriptionSyntax;
+import com.soloplan.oss.sonarqube.plugin.resharper.clt.enumerations.SonarQubeSeverity;
+import org.jetbrains.annotations.NotNull;
+import org.sonar.api.rule.RuleStatus;
+import org.sonar.api.rules.RuleType;
+
+/**
+ * A model class that provides an abbreviated abstraction over all properties that can be set when creating new SonarQube rules, including
+ * some safeguards to prevent runtime exceptions being thrown by the SonarQube instance when importing new rules that would use obsolete or
+ * deprecated values or invalid combinations (e.g. setting HTML and MarkDown descriptions for the same rule).
+ */
+public final class SonarQubeRuleDefinitionModel {
+
+  // region Member variables
+
+  /** The unique identifier of the SonarQube rule definition. */
+  private final String ruleDefinitionKey;
+
+  /** The type of the SonarQube rule definition. */
+  private RuleType ruleType = RuleType.BUG;
+
+  /** The name of the rule definition as displayed within SonarQube. */
+  private String ruleName = "";
+
+  /** The human-readable description of the rule definition as displayed within SonarQube. */
+  private String ruleDescription = "";
+
+  /** The formatting syntax of the description of the rule definition. */
+  private SonarQubeRuleDescriptionSyntax ruleDescriptionSyntax = SonarQubeRuleDescriptionSyntax.getDefaultRuleDescriptionSyntax();
+
+  /** The SonarQube compatible severity value of the rule definition. */
+  private SonarQubeSeverity sonarQubeSeverity = SonarQubeSeverity.getDefaultSeverity();
+
+  /** The status of the SonarQube rule definition. */
+  private RuleStatus ruleStatus = RuleStatus.defaultStatus();
+
+  /** Indicates, whether the SonarQube rule definition is activated by default or not. */
+  private boolean activatedByDefault = false;
+
+  // TODO Create a property for 'Rule tags', that accepts only strings containin the following characters: a-z, 0-9, '+', '-', '#', '.'
+
+  // endregion
+
+  /**
+   * Creates a new instance of the {@link SonarQubeRuleDefinitionModel} class using the supplied {@code ruleDefinitionKey} to uniquely
+   * identify the new instance.
+   *
+   * @param ruleDefinitionKey
+   *     The identifier to uniquely identify the corresponding rule definition within SonarQube. Leading and trailing whitespace characters
+   *     are removed from the supplied value. Must not be {@code null}.
+   */
+  public SonarQubeRuleDefinitionModel(@NotNull String ruleDefinitionKey) {
+    this.ruleDefinitionKey = ruleDefinitionKey.trim();
+  }
+
+  /**
+   * Gets the identifier of this rule which is used to uniquely identify this rule within SonarQube.
+   *
+   * @return The unique identifier of this rule.
+   */
+  public String getRuleDefinitionKey() {
+    return ruleDefinitionKey;
+  }
+
+  /**
+   * Gets the rule type of this rule definition which corresponds to the characteristics of the following table.
+   * <table>
+   * <tr><th>Rule type</th><th>Characteristic</th></tr>
+   * <tr><td>{@link RuleType#CODE_SMELL}</td><td>Maintainability</td></tr>
+   * <tr><td>{@link RuleType#BUG}</td><td>Reliability</td></tr>
+   * <tr><td>{@link RuleType#VULNERABILITY}</td><td>Vulnerability</td></tr>
+   * </table>
+   * <p>
+   * Defaults to {@link RuleType#BUG} if not explicitly set via call to {@link #setRuleType(RuleType)}.
+   * </p>
+   *
+   * @return The type to set for this rule definition.
+   *
+   * @see <a href="https://jira.sonarsource.com/browse/MMF-184">[MMF-184] SonarQube Quality Model with three characteristics -
+   *     SonarSource</a>
+   * @see <a href="https://docs.sonarqube.org/display/SONAR/Rules+-+types+and+severities">Rules - types and severities - SonarQube
+   *     Documentation - Doc SonarQube</a>
+   */
+  public RuleType getRuleType() {
+    return ruleType;
+  }
+
+  /**
+   * Sets the rule type of this rule definition which corresponds to the characteristics of the following table.
+   * <table>
+   * <tr><th>Rule type</th><th>Characteristic</th></tr>
+   * <tr><td>{@link RuleType#CODE_SMELL}</td><td>Maintainability</td></tr>
+   * <tr><td>{@link RuleType#BUG}</td><td>Reliability</td></tr>
+   * <tr><td>{@link RuleType#VULNERABILITY}</td><td>Vulnerability</td></tr>
+   * </table>
+   *
+   * @param ruleType
+   *     The type to set for this rule definition.
+   *
+   * @see <a href="https://jira.sonarsource.com/browse/MMF-184">[MMF-184] SonarQube Quality Model with three characteristics -
+   *     SonarSource</a>
+   * @see <a href="https://docs.sonarqube.org/display/SONAR/Rules+-+types+and+severities">Rules - types and severities - SonarQube
+   *     Documentation - Doc SonarQube</a>
+   */
+  public void setRuleType(@NotNull RuleType ruleType) {
+    this.ruleType = ruleType;
+  }
+
+  /**
+   * Gets the human-readable name of this rule which will be displayed within SonarQube.
+   *
+   * @return The human-readable name of this rule which will be displayed within SonarQube.
+   */
+  public String getRuleName() {
+    return ruleName;
+  }
+
+  /**
+   * Sets the human-readable name of this rule to be displayed within SonarQube. Passing {@code null} as argument will result in an empty
+   * string, leading and trailing whitespace characters will be removed from the supplied argument.
+   *
+   * @param ruleName
+   *     The human-readable name of this rule which will be displayed within SonarQube.
+   */
+  public void setRuleName(String ruleName) {
+    this.ruleName = ruleName == null ? "" : ruleName.trim();
+  }
+
+  /**
+   * Gets a human-readable text that describes the purpose of this rule in detail. The text might be formatted using either HTML or MarkDown
+   * syntax, as defined by the {@link SonarQubeRuleDescriptionSyntax} of this class.
+   *
+   * @return Returns a human-readable text that describes the purpose of this rule.
+   */
+  public String getRuleDescription() {
+    return ruleDescription;
+  }
+
+  /**
+   * Sets a human-readable text that describes the purpose of this rule in detail. The text might be formatted using either HTML or MarkDown
+   * syntax, as defined by the {@link SonarQubeRuleDescriptionSyntax} of this class.
+   * <p>
+   * Note: SonarQube provides specific methods for descriptions using HTML and MarkDown syntax when creating a new rule definition, but
+   * supplying both leads to a runtime exception, hence the abstraction using the {@link SonarQubeRuleDescriptionSyntax} enumeration.
+   * </p>
+   *
+   * @param ruleDescription
+   *     The human-readable text that describes the purpose of this rule in detail. Passing {@code null} as argument will result in an empty
+   *     string, leading and trailing whitespace characters will be removed from the supplied argument.
+   * @param ruleDescriptionSyntax
+   *     Defines the syntax used within the argument passes to parameter {@code ruleDescription}.
+   */
+  public void setRuleDescription(String ruleDescription, @NotNull SonarQubeRuleDescriptionSyntax ruleDescriptionSyntax) {
+    this.ruleDescription = ruleDescription == null ? "" : ruleDescription.trim();
+    this.ruleDescriptionSyntax = ruleDescriptionSyntax;
+  }
+
+  /**
+   * Gets the formatting syntax used within the rule description.
+   * <p>
+   * Defaults to {@link SonarQubeRuleDescriptionSyntax#HTML} if not explicitly set via call to {@link #setRuleDescription(String,
+   * SonarQubeRuleDescriptionSyntax)}.
+   * </p>
+   *
+   * @return The formatting syntax used within the value returned by {@link #getRuleDescription()}.
+   *
+   * @see #setRuleDescription(String, SonarQubeRuleDescriptionSyntax)
+   */
+  public SonarQubeRuleDescriptionSyntax getRuleDescriptionSyntax() {
+    return ruleDescriptionSyntax;
+  }
+
+  /**
+   * Gets the SonarQube compatible severity value of this rule.
+   * <p>
+   * Defaults to {@link SonarQubeSeverity#Major} if not explicitly set via call to {@link #setSonarQubeSeverity(SonarQubeSeverity)}.
+   * </p>
+   *
+   * @return The SonarQube compatible severity value of this rule.
+   */
+  public SonarQubeSeverity getSonarQubeSeverity() {
+    return sonarQubeSeverity;
+  }
+
+  /**
+   * Sets the SonarQube compatible severity value of this rule.
+   * <p>
+   * Although the SonarQube API defines the severity as parameter of class {@link String}, not all values passed in as arguments are valid
+   * and may result in runtime exceptions. This abstraction uses the enumeration {@link SonarQubeSeverity} to ensure, that only valid values
+   * will be used when creating a new rule.
+   * </p>
+   *
+   * @param sonarQubeSeverity
+   *     The SonarQube compatible severity value for this rule.
+   */
+  public void setSonarQubeSeverity(@NotNull SonarQubeSeverity sonarQubeSeverity) {
+    this.sonarQubeSeverity = sonarQubeSeverity;
+  }
+
+  /**
+   * Gets the status of this rule.
+   * <p>
+   * Defaults to {@link RuleStatus#READY} if not explicitly set via call to {@link #setRuleStatus(RuleStatus)}.
+   * </p>
+   *
+   * @return The status of this rule.
+   */
+  public RuleStatus getRuleStatus() {
+    return ruleStatus;
+  }
+
+  /**
+   * Sets the status of this rule. Might throw an {@link IllegalArgumentException} if {@code ruleStatus} is set to {@link
+   * RuleStatus#REMOVED} since this value is no longer supported by SonarQube.
+   *
+   * @param ruleStatus
+   *     The new status of this rule.
+   *
+   * @throws IllegalArgumentException
+   *     If the supplied {@code ruleStatus} matches {@link RuleStatus#REMOVED}, because this value is no longer supported by SonarQube.
+   */
+  public void setRuleStatus(@NotNull RuleStatus ruleStatus)
+      throws IllegalArgumentException {
+    if (ruleStatus == RuleStatus.REMOVED) {
+      throw new IllegalArgumentException("Rule status " + RuleStatus.REMOVED + " is no longer supported by SonarQube.");
+    }
+    this.ruleStatus = ruleStatus;
+  }
+
+  /**
+   * Gets a value indicating whether the rule should be activated by default or not.
+   * <p>
+   * Defaults to {@code false} if not explicitly set via call to {@link #setActivatedByDefault(boolean)}.
+   * </p>
+   *
+   * @return {@code true} if the rule should be activated by default, otherwise {@code false}.
+   */
+  public boolean isActivatedByDefault() {
+    return activatedByDefault;
+  }
+
+  /**
+   * Sets a value indicating whether the rule should be activated by default or not.
+   *
+   * @param activatedByDefault
+   *     {@code true} if the rule should be activated by default, otherwise {@code false}.
+   */
+  public void setActivatedByDefault(boolean activatedByDefault) {
+    this.activatedByDefault = activatedByDefault;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+
+    return ruleDefinitionKey.equals(((SonarQubeRuleDefinitionModel) other).ruleDefinitionKey);
+  }
+
+  @Override
+  public int hashCode() {
+    return ruleDefinitionKey.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return "SonarQubeRuleDefinitionModel{" +
+        "ruleDefinitionKey='" + ruleDefinitionKey + '\'' +
+        ", ruleType=" + ruleType +
+        ", ruleName='" + ruleName + '\'' +
+        ", ruleDescription='" + ruleDescription + '\'' +
+        ", ruleDescriptionSyntax=" + ruleDescriptionSyntax +
+        ", sonarQubeSeverity=" + sonarQubeSeverity +
+        ", ruleStatus=" + ruleStatus +
+        ", activatedByDefault=" + activatedByDefault +
+        '}';
+  }
+}
